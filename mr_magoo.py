4f5ae683bfc39867fba0a33590446896e625f5c6
James Murphy <jmurphy@spartaglobal.com>
2020-02-07 08:28:23 +0000

    still fail_code left + mr. magoo except block

--- mr_magoo.py
+++ mr_magoo.py
@@ -1,3 +1,4 @@
+import re
 
 print("Welcome to Mr. Magoo the Sensei, if you are at peace and wish to leave")
 print("please tell him: 'I am at peace'")
@@ -7,9 +8,11 @@ while True:
     if user_response.lower().replace(" ", "") == 'iamatpeace':
         print('Sometimes what heart know, head forget')
         break
-   # elif
+    elif not user_response.lower().startswith('sensei'):
+        print("You are smart but not wise, please address me as Sensei")
     elif "?" in user_response:
         print('Questions are wise but for now wax on and off')
-
+    elif re.search('block*', user_response):  #---> doesn't appear to be working
+        print("Remember, best block, not to be there...")
     else:
         print("Don't lose focus, wax on and off")
